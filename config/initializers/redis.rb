076dda527cb0eff5f2fce354d2a1f660bdceeb9d
Charles Skariah <charles.skariah@imaginea.com>
2019-03-24 22:29:48 +0530

    Heroku Changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-
-$redis = Redis::Namespace.new("thermostat_api", :redis => Redis.new)
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+$redis = Redis::Namespace.new("thermostat_api", :redis => Redis.new(:url => uri))
